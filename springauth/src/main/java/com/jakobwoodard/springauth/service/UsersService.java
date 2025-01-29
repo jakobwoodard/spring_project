d7e4dda4bec7eeb13954f1fe44c606b2bcf97ebc
Jakob Woodard <jakobwoodard@icloud.com>
2025-01-29 16:40:04 -0500

    Updated password encoder to use bcrypt

--- springauth/src/main/java/com/jakobwoodard/springauth/service/UsersService.java
+++ springauth/src/main/java/com/jakobwoodard/springauth/service/UsersService.java
@@ -4,24 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
 
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 = 
-            User.withDefaultPasswordEncoder()
-                .username("username")
-                .password("password")
+            User.withUsername("username")
+                .password("{bcrypt}" + passwordEncoder.encode("password"))
                 .roles("USER")
                 .build();
         UserDetails user2 = 
-            User.withDefaultPasswordEncoder()
-                .username("root")
-                .password("admin")
+            User.withUsername("root")
+                .password("{bcrypt}" + passwordEncoder.encode("admin"))
                 .roles("ADMIN", "USER")
                 .build();
 
